f1f93a695f0f4574c17b95cc860f1d8bebe53a5a
zachmae <zacharie.lawson@epitech.eu>
2022-10-04 16:01:47 +0200

    feat (cmake asio) : able to build the project using asio library

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,13 @@
 */
 
 #include <iostream>
+#include <asio.hpp>
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    asio::io_context io;
+    asio::steady_timer t(io, asio::chrono::seconds(5));
+    t.wait();
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
