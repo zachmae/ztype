2184be539afb466371d30d7d1243406a60ace15b
Erwan Cariou <erwan1.cariou@epitech.eu>
2022-10-16 22:01:38 +0200

    rm(server/ client/): deleting directories server/clients because they are now implemented

--- client/FindSfmlLib.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-set(BUILD_SHARED_LIBS OFF)
-
-FetchContent_Declare(SFML
- GIT_REPOSITORY https://github.com/SFML/SFML.git
- GIT_TAG 8f54a3e97c35476012613a8a41b7d2234d7ce38e
-)
-
-FetchContent_MakeAvailable(SFML)
